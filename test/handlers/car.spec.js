18621658dc921c6754440b3e35b15494b266e0d6
Alan Shaw <alan.shaw@protocol.ai>
2023-05-19 12:05:26 +0100

    feat: implement block order signaling (#25)
    This PR updates dagula and implements block order signaling via HTTP
    Accept header as specified in https://github.com/ipfs/specs/pull/412.
    
    It also renames the query param `car-scope` to `dag-scope` and changes
    the scope value `file` to `entity` as these were updated in
    https://github.com/ipfs/specs/pull/402.

--- test/handlers/car.spec.js
+++ test/handlers/car.spec.js
@@ -30,4 +30,104 @@ describe('CAR handler', () => {
     assert(await reader.get(leafBlock.cid))
     assert(await reader.get(rootBlock.cid))
   })
+
+  it('serves a CAR with query parameter dag-scope=block', async () => {
+    const waitUntil = mockWaitUntil()
+    const leafBlock0 = await encode({ value: fromString('test0'), codec: raw, hasher })
+    const leafBlock1 = await encode({ value: fromString('test1'), codec: raw, hasher })
+    const rootBlock = await encode({ value: { leaf0: leafBlock0.cid, leaf1: leafBlock1.cid }, codec: cbor, hasher })
+    const blockstore = mockBlockstore([leafBlock0, leafBlock1, rootBlock])
+    const dagula = new Dagula(blockstore)
+    const url = new URL(`http://localhost/ipfs/${rootBlock.cid}/leaf0?dag-scope=block`)
+    const path = url.pathname.replace(`/ipfs/${rootBlock.cid}`, '')
+    const ctx = { waitUntil, dagula, dataCid: rootBlock.cid, path, searchParams: url.searchParams }
+    const env = { DEBUG: 'true' }
+    const req = new Request(url)
+    const res = await handleCar(req, env, ctx)
+    const reader = await CarReader.fromBytes(new Uint8Array(await res.arrayBuffer()))
+    const roots = await reader.getRoots()
+    assert.strictEqual(roots[0].toString(), rootBlock.cid.toString())
+    assert(await reader.get(leafBlock0.cid))
+    assert(await reader.get(rootBlock.cid))
+    assert(!(await reader.has(leafBlock1.cid)))
+  })
+
+  it('serves a CAR with accept parameter order=dfs', async () => {
+    const waitUntil = mockWaitUntil()
+
+    const leafBlock0 = await encode({ value: fromString('test0'), codec: raw, hasher })
+    const leafBlock1 = await encode({ value: fromString('test1'), codec: raw, hasher })
+    const branchBlock0 = await encode({ value: { leaf: leafBlock0.cid }, codec: cbor, hasher })
+    const branchBlock1 = await encode({ value: { leaf: leafBlock1.cid }, codec: cbor, hasher })
+    const rootBlock = await encode({ value: [branchBlock0.cid, branchBlock1.cid], codec: cbor, hasher })
+    const blockstore = mockBlockstore([leafBlock0, leafBlock1, branchBlock0, branchBlock1, rootBlock])
+    const dagula = new Dagula(blockstore)
+    const url = new URL(`http://localhost/ipfs/${rootBlock.cid}`)
+    const path = url.pathname.replace(`/ipfs/${rootBlock.cid}`, '')
+    const ctx = { waitUntil, dagula, dataCid: rootBlock.cid, path, searchParams: url.searchParams }
+    const env = { DEBUG: 'true' }
+    const req = new Request(url, {
+      headers: {
+        Accept: 'application/vnd.ipld.car; order=dfs'
+      }
+    })
+    const res = await handleCar(req, env, ctx)
+    const contentType = res.headers.get('Content-Type')
+    assert(contentType)
+    assert(contentType.includes('order=dfs'))
+
+    const reader = await CarReader.fromBytes(new Uint8Array(await res.arrayBuffer()))
+    const roots = await reader.getRoots()
+    assert.strictEqual(roots[0].toString(), rootBlock.cid.toString())
+
+    const blocks = []
+    for await (const b of reader.blocks()) {
+      blocks.push(b)
+    }
+    assert.strictEqual(blocks[0].cid.toString(), rootBlock.cid.toString())
+    assert.strictEqual(blocks[1].cid.toString(), branchBlock0.cid.toString())
+    assert.strictEqual(blocks[2].cid.toString(), leafBlock0.cid.toString())
+    assert.strictEqual(blocks[3].cid.toString(), branchBlock1.cid.toString())
+    assert.strictEqual(blocks[4].cid.toString(), leafBlock1.cid.toString())
+  })
+
+  // unk = unknown, so underlying implementation could change in the future
+  it('serves a CAR with accept parameter order=unk', async () => {
+    const waitUntil = mockWaitUntil()
+
+    const leafBlock0 = await encode({ value: fromString('test0'), codec: raw, hasher })
+    const leafBlock1 = await encode({ value: fromString('test1'), codec: raw, hasher })
+    const branchBlock0 = await encode({ value: { leaf: leafBlock0.cid }, codec: cbor, hasher })
+    const branchBlock1 = await encode({ value: { leaf: leafBlock1.cid }, codec: cbor, hasher })
+    const rootBlock = await encode({ value: [branchBlock0.cid, branchBlock1.cid], codec: cbor, hasher })
+    const blockstore = mockBlockstore([leafBlock0, leafBlock1, branchBlock0, branchBlock1, rootBlock])
+    const dagula = new Dagula(blockstore)
+    const url = new URL(`http://localhost/ipfs/${rootBlock.cid}`)
+    const path = url.pathname.replace(`/ipfs/${rootBlock.cid}`, '')
+    const ctx = { waitUntil, dagula, dataCid: rootBlock.cid, path, searchParams: url.searchParams }
+    const env = { DEBUG: 'true' }
+    const req = new Request(url, {
+      headers: {
+        Accept: 'application/vnd.ipld.car; order=unk'
+      }
+    })
+    const res = await handleCar(req, env, ctx)
+    const contentType = res.headers.get('Content-Type')
+    assert(contentType)
+    assert(contentType.includes('order=unk'))
+
+    const reader = await CarReader.fromBytes(new Uint8Array(await res.arrayBuffer()))
+    const roots = await reader.getRoots()
+    assert.strictEqual(roots[0].toString(), rootBlock.cid.toString())
+
+    const blocks = []
+    for await (const b of reader.blocks()) {
+      blocks.push(b)
+    }
+    assert.strictEqual(blocks[0].cid.toString(), rootBlock.cid.toString())
+    assert.strictEqual(blocks[1].cid.toString(), branchBlock0.cid.toString())
+    assert.strictEqual(blocks[2].cid.toString(), branchBlock1.cid.toString())
+    assert.strictEqual(blocks[3].cid.toString(), leafBlock0.cid.toString())
+    assert.strictEqual(blocks[4].cid.toString(), leafBlock1.cid.toString())
+  })
 })
